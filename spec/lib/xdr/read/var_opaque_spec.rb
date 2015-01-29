450cb67eed87952fd2d1c69b34adf4da48586f51
Scott Fleckenstein <nullstyle@gmail.com>
2015-01-28 18:42:18 -0800

    Add invalid length spec for VarOpaque

--- spec/lib/xdr/read/var_opaque_spec.rb
+++ spec/lib/xdr/read/var_opaque_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 
 describe XDR::Read::VarOpaque, "#read" do
-  subject{ XDR::Read::VarOpaque.new(6) }
+  subject{ XDR::Read::VarOpaque.new(2) }
 
   it "decodes values correctly" do
     expect(read("\x00\x00\x00\x00")).to eq("")
     expect(read("\x00\x00\x00\x01\x00\x00\x00\x00")).to eq("\x00")
     expect(read("\x00\x00\x00\x01\x01\x00\x00\x00")).to eq("\x01")
     expect(read("\x00\x00\x00\x02\x00\x01\x00\x00")).to eq("\x00\x01")
+  end
+
+  it "raises a ReadError when the encoded length is greater than the allowed max" do
+    expect{ read "\x00\x00\x00\x03\x00\x00\x00\x00" }.to raise_error(XDR::ReadError)
   end
 
   def read(str)
