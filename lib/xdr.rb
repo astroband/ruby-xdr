ef80fcb113721ddce93884b7b1eea86de9dd01c1
Scott Fleckenstein <nullstyle@gmail.com>
2015-01-30 12:07:19 -0800

    Add Logger helper

--- lib/xdr.rb
+++ lib/xdr.rb
@@ -1,6 +1,8 @@
 require "xdr/version"
 require "active_support/dependencies/autoload"
 require "active_support/core_ext/object/blank"
+require "active_support/core_ext/module/attribute_accessors"
+require "active_support/logger"
 
 module XDR
   extend ActiveSupport::Autoload
@@ -16,4 +18,8 @@ module XDR
   class Error < StandardError ; end
   class ReadError < Error ; end
   class EnumValueError < ReadError ; end
+
+  mattr_accessor :logger
+  self.logger = ActiveSupport::Logger.new(STDOUT)
+  self.logger.level = Logger::WARN
 end
