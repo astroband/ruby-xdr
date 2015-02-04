d8ab11003e2af230cbb720d1f5a793bde3413c99
Scott Fleckenstein <nullstyle@gmail.com>
2015-02-03 19:12:34 -0800

    Add XDR::Union.switch_name

--- lib/xdr/union.rb
+++ lib/xdr/union.rb
@@ -9,10 +9,12 @@ class XDR::Union
   class_attribute :arms
   class_attribute :switches
   class_attribute :switch_type
+  class_attribute :switch_name
 
   self.arms        = ActiveSupport::OrderedHash.new
   self.switches    = ActiveSupport::OrderedHash.new
   self.switch_type = nil
+  self.switch_name = nil
 
   validates_with XDR::UnionValidator
 end
