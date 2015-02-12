e06ef199034df4d5639b7244830ecc547ff61035
Scott Fleckenstein <nullstyle@gmail.com>
2015-02-11 17:55:38 -0800

    Add coverage for VarOpaque

--- spec/lib/xdr/var_opaque_spec.rb
+++ spec/lib/xdr/var_opaque_spec.rb
@@ -20,3 +20,24 @@ describe XDR::VarOpaque, "#read" do
     subject.read(io)
   end
 end
+
+describe XDR::VarOpaque, "#write" do
+  subject{ XDR::VarOpaque[2] }
+
+  it "encodes values correctly" do
+    expect(write("")).to eq("\x00\x00\x00\x00")
+    expect(write("\x00")).to eq("\x00\x00\x00\x01\x00\x00\x00\x00")
+    expect(write("\x01")).to eq("\x00\x00\x00\x01\x01\x00\x00\x00")
+    expect(write("\x00\x01")).to eq("\x00\x00\x00\x02\x00\x01\x00\x00")
+  end
+
+  it "raises a WriteError when the provided string is too long" do
+    expect{ write "123" }.to raise_error(XDR::WriteError)
+  end
+
+  def write(val)
+    io = StringIO.new()
+    subject.write(val, io)
+    io.string
+  end
+end
