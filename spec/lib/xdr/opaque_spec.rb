80b7c303211ec4b3bf6b2e5171106f77e853f9d0
Scott Fleckenstein <nullstyle@gmail.com>
2015-02-11 17:52:53 -0800

    Add coverage for Opaque

--- spec/lib/xdr/opaque_spec.rb
+++ spec/lib/xdr/opaque_spec.rb
@@ -14,3 +14,23 @@ describe XDR::Opaque, "#read" do
     subject.read(io)
   end
 end
+
+describe XDR::Opaque, "#write" do
+  subject{ XDR::Opaque.new(3) }
+
+  it "encodes values correctly" do
+    expect(write("123")).to eq("123\x00")
+    expect(write("124")).to eq("124\x00")
+  end
+
+  it "raises a WriteError if the value is not the correct length" do
+    expect{ write("1234") }.to raise_error(XDR::WriteError)
+    expect{ write("12") }.to raise_error(XDR::WriteError)
+  end
+
+  def write(val)
+    io = StringIO.new()
+    subject.write(val, io)
+    io.string
+  end
+end
